4172c96913cb2d5ae0372ec97895e90d31100b65
GurnoorSohal <sohalgurnoor@gmail.com>
2025-02-15 15:25:37 -0800

    update main.cpp

--- IOT/src/main.cpp
+++ IOT/src/main.cpp
@@ -3,9 +3,10 @@
 
 // MQTT client - using descriptive client ID and topic
 #define CLIENT_ID "esp32-sensors"
-#define TOPIC_PREFIX "ENTER_SOMETHING_UNIQUE_HERE_THAT_SHOULD_ALSO_MATCH_MAINPY/ece140/sensors"
+#define TOPIC_PREFIX "gurnoor/ece140/sensors"
 
 ECE140_MQTT mqtt(CLIENT_ID, TOPIC_PREFIX);
+ECE140_WIFI wifi;
 
 // WiFi credentials
 const char* ucsdUsername = UCSD_USERNAME;
@@ -13,8 +14,12 @@ const char* ucsdPassword = UCSD_PASSWORD;
 const char* wifiSsid = WIFI_SSID;
 const char* nonEnterpriseWifiPassword = NON_ENTERPRISE_WIFI_PASSWORD;
 
+unsigned long lsatPublish = 0;
+
 void setup() {
     Serial.begin(115200);
+    delay(1000);
+    Serial.println("Starting ESP32 sensor node...")
 }
 
 void loop() {
